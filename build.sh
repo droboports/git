c88514fae3f05154d3b797b1073b83a44e26ab03
Ricardo Padilha <ricardospadilha@gmail.com>
2014-09-23 16:54:25 +0200

    First version

---
+++ build.sh
@@ -0,0 +1,174 @@
+#!/usr/bin/env bash
+
+### bash best practices ###
+# exit on error code
+set -o errexit
+# exit on unset variable
+set -o nounset
+# return error of last failed command in pipe
+set -o pipefail
+# print trace
+set -o xtrace
+
+### logfile ###
+timestamp="$(date +%Y-%m-%d_%H-%M-%S)"
+logfile="logfile_${timestamp}.txt"
+echo "${0} ${@}" > "${logfile}"
+# save stdout to logfile
+exec 1> >(tee -a "${logfile}")
+# redirect errors to stdout
+exec 2> >(tee -a "${logfile}" >&2)
+
+### environment variables ###
+. crosscompile.sh
+export NAME="git"
+export DEST="/mnt/DroboFS/Shares/DroboApps/${NAME}"
+export DEPS="/mnt/DroboFS/Shares/DroboApps/${NAME}deps"
+export CFLAGS="${CFLAGS:-} -Os -fPIC"
+export CXXFLAGS="${CXXFLAGS:-} ${CFLAGS}"
+export CPPFLAGS="-I${DEPS}/include"
+export LDFLAGS="${LDFLAGS:-} -Wl,-rpath,${DEST}/lib -L${DEST}/lib"
+alias make="make -j8  V=1 VERBOSE=1"
+
+# $1: file
+# $2: url
+# $3: folder
+_download_tgz() {
+  [[ ! -f "download/${1}" ]] && wget -O "download/${1}" "${2}"
+  [[ -d "target/${3}" ]]   && rm -v -fr "target/${3}"
+  [[ ! -d "target/${3}" ]] && tar -zxvf "download/${1}" -C target
+}
+
+### ZLIB ###
+_build_zlib() {
+local VERSION="1.2.8"
+local FOLDER="zlib-${VERSION}"
+local FILE="${FOLDER}.tar.gz"
+local URL="http://zlib.net/${FILE}"
+
+_download_tgz "${FILE}" "${URL}" "${FOLDER}"
+pushd target/"${FOLDER}"
+./configure --prefix="${DEPS}" --libdir="${DEST}/lib"
+make
+make install
+rm -v "${DEST}/lib"/*.a
+popd
+}
+
+### OPENSSL ###
+_build_openssl() {
+local VERSION="1.0.1i"
+local FOLDER="openssl-${VERSION}"
+local FILE="${FOLDER}.tar.gz"
+local URL="http://www.openssl.org/source/${FILE}"
+
+_download_tgz "${FILE}" "${URL}" "${FOLDER}"
+pushd target/"${FOLDER}"
+./Configure --prefix="${DEPS}" \
+  --openssldir="${DEST}/etc/ssl" \
+  --with-zlib-include="${DEPS}/include" \
+  --with-zlib-lib="${DEPS}/lib" \
+  shared zlib-dynamic threads linux-armv4 -DL_ENDIAN ${CFLAGS} ${LDFLAGS}
+sed -i -e "s/-O3//g" Makefile
+make -j1
+make install_sw
+mkdir -p "${DEST}/libexec"
+cp -v -a "${DEPS}/bin/openssl" "${DEST}/libexec/"
+cp -v -aR "${DEPS}/lib"/* "${DEST}/lib/"
+rm -v -fr "${DEPS}/lib"
+rm -v "${DEST}/lib"/*.a
+sed -i -e "s|^exec_prefix=.*|exec_prefix=${DEST}|g" "${DEST}/lib/pkgconfig/openssl.pc"
+popd
+}
+
+### CURL ###
+_build_curl() {
+local VERSION="7.38.0"
+local FOLDER="curl-${VERSION}"
+local FILE="${FOLDER}.tar.gz"
+local URL="http://curl.haxx.se/download/${FILE}"
+
+_download_tgz "${FILE}" "${URL}" "${FOLDER}"
+pushd target/"${FOLDER}"
+./configure --host=arm-none-linux-gnueabi --prefix="${DEPS}" --libdir="${DEST}/lib" --disable-static --disable-debug --disable-curldebug --with-ssl --with-zlib --with-random --with-ca-bundle=$DEST/etc/ssl/certs/ca-certificates.crt
+make
+make install
+popd
+}
+
+### EXPAT ###
+_build_expat() {
+local VERSION="2.1.0"
+local FOLDER="expat-${VERSION}"
+local FILE="${FOLDER}.tar.gz"
+local URL="http://switch.dl.sourceforge.net/project/expat/expat/2.1.0/${FILE}"
+
+_download_tgz "${FILE}" "${URL}" "${FOLDER}"
+pushd target/"${FOLDER}"
+./configure --host=arm-none-linux-gnueabi --prefix="${DEPS}" --libdir="${DEST}/lib" --disable-static
+make
+make install
+popd
+}
+
+### GIT ###
+_build_git() {
+local VERSION="2.1.1"
+local FOLDER="git-${VERSION}"
+local FILE="${FOLDER}.tar.gz"
+local URL="https://www.kernel.org/pub/software/scm/git/${FILE}"
+
+_download_tgz "${FILE}" "${URL}" "${FOLDER}"
+pushd target/"${FOLDER}"
+./configure --host=arm-none-linux-gnueabi --prefix="${DEST}" --mandir="${DEST}/man" --with-openssl --with-curl --with-expat ac_cv_fread_reads_directories=no ac_cv_snprintf_returns_bogus=no ac_cv_lib_curl_curl_global_init=yes
+make
+make install
+popd
+}
+
+### BUILD ###
+_build() {
+  _build_zlib
+  _build_openssl
+  _build_curl
+  _build_expat
+  _build_git
+  _package
+}
+
+_create_tgz() {
+  local appname="$(basename $(pwd))"
+  local appfile="${appname}.tgz"
+
+  if [[ -f "${appfile}" ]]; then
+    rm -v "${appfile}"
+  fi
+
+  tar --verbose --create --numeric-owner --owner=0 --group=0 --gzip --file "${appfile}" -C "${DEST}" .
+}
+
+_package() {
+  cp -v -faR src/dest/* "${DEST}"/
+  find "${DEST}" -name "._*" -print -delete
+  _create_tgz
+}
+
+_clean() {
+  rm -v -fr "${DEPS}"
+  rm -v -fr "${DEST}"
+  rm -v -fr target/*
+}
+
+_dist_clean() {
+  _clean
+  rm -v -f logfile*
+  rm -v -fr download/*
+}
+
+case "${1:-}" in
+  clean)     _clean ;;
+  distclean) _dist_clean ;;
+  package)   _package ;;
+  "")        _build ;;
+  *)         _build_${1} ;;
+esac
