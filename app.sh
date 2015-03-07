19bf7705af704d212b4142b26313f83d7ad4bd13
Ricardo Padilha <ricardospadilha@gmail.com>
2015-03-06 22:10:44 -0300

    Git 2.3.2 with openssl parallel build

--- app.sh
+++ app.sh
@@ -16,20 +16,22 @@ popd
 
 ### OPENSSL ###
 _build_openssl() {
-local VERSION="1.0.1l"
+local VERSION="1.0.2"
 local FOLDER="openssl-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
 local URL="http://www.openssl.org/source/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
-pushd target/"${FOLDER}"
+cp -vf src/openssl-1.0.2-parallel-build.patch "target/${FOLDER}/"
+pushd "target/${FOLDER}"
+patch -p1 < openssl-1.0.2-parallel-build.patch
 ./Configure --prefix="${DEPS}" \
   --openssldir="${DEST}/etc/ssl" \
   --with-zlib-include="${DEPS}/include" \
   --with-zlib-lib="${DEPS}/lib" \
   shared zlib-dynamic threads linux-armv4 -DL_ENDIAN ${CFLAGS} ${LDFLAGS}
 sed -i -e "s/-O3//g" Makefile
-make -j1
+make
 make install_sw
 mkdir -p "${DEST}/libexec"
 cp -v -a "${DEPS}/bin/openssl" "${DEST}/libexec/"
@@ -42,13 +44,13 @@ popd
 
 ### CURL ###
 _build_curl() {
-local VERSION="7.40.0"
+local VERSION="7.41.0"
 local FOLDER="curl-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
 local URL="http://curl.haxx.se/download/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
-pushd target/"${FOLDER}"
+pushd "target/${FOLDER}"
 ./configure --host="${HOST}" --prefix="${DEPS}" --libdir="${DEST}/lib" --disable-static --disable-debug --disable-curldebug --with-zlib="${DEPS}" --with-ssl="${DEPS}" --with-random --with-ca-bundle="${DEST}/etc/ssl/certs/ca-certificates.crt" --enable-ipv6
 make
 make install
@@ -63,7 +65,7 @@ local FILE="${FOLDER}.tar.gz"
 local URL="http://switch.dl.sourceforge.net/project/expat/expat/2.1.0/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
-pushd target/"${FOLDER}"
+pushd "target/${FOLDER}"
 ./configure --host=arm-none-linux-gnueabi --prefix="${DEPS}" --libdir="${DEST}/lib" --disable-static
 make
 make install
@@ -72,13 +74,13 @@ popd
 
 ### GIT ###
 _build_git() {
-local VERSION="2.2.2"
+local VERSION="2.3.2"
 local FOLDER="git-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
 local URL="https://www.kernel.org/pub/software/scm/git/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
-pushd target/"${FOLDER}"
+pushd "target/${FOLDER}"
 ./configure --host=arm-none-linux-gnueabi --prefix="${DEST}" --mandir="${DEST}/man" --with-openssl --with-curl --with-expat ac_cv_fread_reads_directories=no ac_cv_snprintf_returns_bogus=no ac_cv_lib_curl_curl_global_init=yes
 make
 make install
